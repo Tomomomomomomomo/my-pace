d7719b4517ae4e4d98c7cb7d7ae881c5cb61f279
WataruOhkura <wohkura@nexseed.net>
2025-08-14 10:41:36 +0800

    Taskモデルとコントーラーを作りました。
    Taskコントローラーには
    〇〇というメソッドを作って、〇〇の処理をしたあと、〇〇のページに遷移するようにしています。

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,6 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/my_pace', [My_paceController::class, 'destroy'])->name('my_pace.destroy');
-
 });
 
 require __DIR__.'/auth.php';
