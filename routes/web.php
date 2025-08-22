2d2757f558491def6429b3675e0bd1f8f7989bcc
Tomomomomomomomo <yt0908@ezweb.ne.jp>
2025-08-22 09:11:46 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    
 });
 
 require __DIR__.'/auth.php';
